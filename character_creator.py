fdec3f037363ceecb151a9f10be3d00339a00cf9
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2025-11-06 11:49:15 -0600

    Update character_creator.py
    Added more functionality to the fallout branches.

--- character_creator.py
+++ character_creator.py
@@ -23,6 +23,7 @@ while True:
     print("That is not a valid response.")
 
 #Fallout character creation
+#SPECIAL stats dictionary
 stats_dict = {
     "Strength":0,
     "Perception":0,
@@ -32,13 +33,19 @@ stats_dict = {
     "Agility": 0,
     "Luck": 0
     }
+#SPECIAL stats total values
+special_total_older = 40
+special_total_4 = 28
+
+#Fallout trees
 if game_choice.lower() == "fallout":
+  print("Are you playing Fallout 4? Yes or No: ")
+  user_fallout_game = str(input())
   print("Please choose your character's name: ")
   character_name = str(input())
   print(f"Your character's name is: {character_name}")
-  #print("Please choose your level of Strength\nfrom 1-10: ")
-  #stats_dict["strength"] = int(input())
 
+  #updating SPECIAL stats per user input
   for key in list(stats_dict.keys()):
     current_value = stats_dict[key]
     print(f"Please choose your amount of {key}: ")
@@ -55,24 +62,29 @@ if game_choice.lower() == "fallout":
         print("Please select a number value.")
         updated_stat = int(input())
   
-  print(f"Your final stats are: {stats_dict}")
+  #User's sum after their choices
+  user_total_sum = sum(stats_dict.values())
+  #checking total amount for Fallout 4     
+  if user_fallout_game.lower() == "yes":
+    if user_total_sum == special_total_4:
+      print(f"Your current stats are: {stats_dict}")
+    else:
+      difference_new = special_total_4 - user_total_sum
+      print(f"You still have {difference_new} points to allocate.")
+      print(f"Please add/subtract values until you have a total of {special_total_4}.")
+  #checking total amount for other games
+  if user_fallout_game.lower() == "no":
+    if user_total_sum == special_total_older:
+      print(f"Your current stats are: {stats_dict}")
+    else:
+      difference_old = (special_total_older - user_total_sum)
+      print(f"You still have {difference_old} points to allocate.")
+      print(f"Please add/subtract values until you have a total of {special_total_older}.")
   
   
   
+  #prints final stats list
+  print(f"Your current stats are: {stats_dict}")
   
   
   
-  """print("Please choose your level of Perception: ")
-  perception = int(input())
-  while True:
-    try:
-      if 1 <= perception <= 10:
-        print(f"Your perception level is: {perception}")
-        break
-      else:
-        print("Please make your selection between 1 and 10: ")
-        perception = int(input())
-    except Exception:
-      print("Please select a number value.")
-      perception = int(input())"""
-
